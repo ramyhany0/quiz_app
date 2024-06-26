7b903a815fa21fca9f5f9ec5c497443ccfb22b1f
ramyhany0 <ramyhany161@gmail.com>
2024-06-26 22:31:29 +0300

    "Updated home screen to center content, added logo image, and 'Learn Flutter' text"

--- lib/home.dart
+++ lib/home.dart
@@ -10,14 +10,26 @@ class homeApp extends StatefulWidget {
 class _homeAppState extends State<homeApp> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          "assets/images/quiz-logo.png",
-          width: 300,
-        )
-      ],
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Learn Flutter",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
