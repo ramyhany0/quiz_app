117c43ffb563e52afe26ff7b474e56d40ccf121b
ramyhany0 <ramyhany161@gmail.com>
2024-06-26 22:05:54 +0300

    inite File

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class homeApp extends StatefulWidget {
+  const homeApp({super.key});
+
+  @override
+  State<homeApp> createState() => _homeAppState();
+}
+
+class _homeAppState extends State<homeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/home.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient:
+                    LinearGradient(colors: [Colors.blue, Colors.deepPurple])),
+            child: const homeApp()),
+      ),
+    );
+  }
+}
