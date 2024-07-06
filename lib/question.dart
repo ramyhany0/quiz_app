5f7085ba2fec2665e5baa6f8f26c76676ecef4bd
ramyhany0 <ramyhany161@gmail.com>
2024-07-06 21:57:03 +0300

    "Added quiz questions and implemented question UI with buttons"

--- lib/question.dart
+++ lib/question.dart
@@ -10,6 +10,58 @@ class Question extends StatefulWidget {
 class _QuestionsState extends State<Question> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Quesyion1...',
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.grey.shade800,
+            ),
+            onPressed: () {},
+            child: const Text(
+              "Answer",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.grey.shade800,
+            ),
+            onPressed: () {},
+            child: const Text(
+              "Answer",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.grey.shade800,
+            ),
+            onPressed: () {},
+            child: const Text(
+              "Answer",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
