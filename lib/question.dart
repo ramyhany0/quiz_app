54ac30889418b1a5fff879097c9bf529e0d2603d
ramyhany0 <ramyhany161@gmail.com>
2024-07-30 11:55:05 +0300

    Add Q1

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/models/answer_button.dart';
 
 class Question extends StatefulWidget {
@@ -9,6 +10,7 @@ class Question extends StatefulWidget {
 }
 
 class _QuestionsState extends State<Question> {
+  final CurrenQuestion = questions[0];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -16,8 +18,8 @@ class _QuestionsState extends State<Question> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Quesyion1...',
+          Text(
+            CurrenQuestion.text,
           ),
           const SizedBox(
             height: 40,
